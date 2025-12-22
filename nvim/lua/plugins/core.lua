f11c6b45a9afb137a2228b93fb41a1c7c46af4a8
niux3 <renaudbourdeau@gmail.com>
2025-12-22 18:09:55 +0100

    change name plugin

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,40 +1,40 @@
 return {
   -- Désactiver les onglets (bufferline)
-  { "akinsho/bufferline.nvim", enabled = false  },
+  { "akinsho/bufferline.nvim", enabled = false },
 
   -- Désactiver les icônes si tu veux
-  { "nvim-tree/nvim-web-devicons", enabled = true  },
+  { "nvim-tree/nvim-web-devicons", enabled = true },
   {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
-        virtual_text = false,      -- le texte inline
-        signs = true,              -- Garde les signes marge
-        underline = true,          -- Garde soulignement
-        update_in_insert = true,  -- update en insertion
-      }
-    }
+        virtual_text = false, -- le texte inline
+        signs = true, -- Garde les signes marge
+        underline = true, -- Garde soulignement
+        update_in_insert = true, -- update en insertion
+      },
+    },
   },
   -- surround
   {
-    "echasnovski/mini.surround",
+    "nvim-mini/mini.surround",
     opts = {
       -- Config options si besoin
       mappings = {
-        add = "ys",           -- Add surrounding
-        delete = "ds",        -- Delete surrounding  
-        find = "",            -- Find surrounding
+        add = "ys", -- Add surrounding
+        delete = "ds", -- Delete surrounding
+        find = "", -- Find surrounding
         find_left = "",
         highlight = "",
-        replace = "cs",       -- Change surrounding
+        replace = "cs", -- Change surrounding
         update_n_lines = "",
-      }
-    }
+      },
+    },
   },
   {
     "olrtg/nvim-emmet",
     config = function()
-      vim.keymap.set({ "n", "v" }, '<C-e>', require('nvim-emmet').wrap_with_abbreviation)
+      vim.keymap.set({ "n", "v" }, "<C-e>", require("nvim-emmet").wrap_with_abbreviation)
     end,
   },
   -- CtrlP
@@ -70,90 +70,88 @@ return {
       filesystem = {
         commands = {
           -- Commande personnalisée si besoin
-        }
-
+        },
       },
       window = {
         mappings = {
           ["s"] = "split_with_window_picker",
-          ["v"] = "vsplit_with_window_picker", 
+          ["v"] = "vsplit_with_window_picker",
           ["S"] = "open_split",
           ["V"] = "open_vsplit",
-
-        }
-      }
-    }
+        },
+      },
+    },
   },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = true  },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = true },
   {
     "folke/snacks.nvim",
     opts = {
       -- Désactiver les barres verticales d'indentation
       indent = {
-        enabled = false,  -- Désactiver complètement
+        enabled = false, -- Désactiver complètement
         -- Ou personnaliser :
         -- char = "│",     -- Changer le caractère
         -- highlight = "Comment",  -- Changer la couleur
       },
       -- Désactiver aussi les autres fonctionnalités si besoin
       scope = {
-        enabled = false,  -- Barres de scope
-      }
-    }
+        enabled = false, -- Barres de scope
+      },
+    },
   },
   {
-    'NvChad/nvim-colorizer.lua',
+    "NvChad/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require('colorizer').setup({
+      require("colorizer").setup({
         filetypes = {
-          'css',
-          'javascript',
-          'html',
-          'lua',
-          'vim',
-          'typescript',
-          'javascriptreact',
-          'typescriptreact',
-          'svelte',
-          'vue',
-          'astro',
-          'xml',
-          'markdown',
-          'php',
-          'python',
-          'scss',
-          'less',
-          'json',
-          'yaml',
+          "css",
+          "javascript",
+          "html",
+          "lua",
+          "vim",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "astro",
+          "xml",
+          "markdown",
+          "php",
+          "python",
+          "scss",
+          "less",
+          "json",
+          "yaml",
         },
         user_default_options = {
           RGB = true,
           RRGGBB = true,
-          names = false,           -- Ne pas afficher les noms (red, blue)
+          names = false, -- Ne pas afficher les noms (red, blue)
           RRGGBBAA = true,
           AARRGGBB = true,
           rgb_fn = true,
           hsl_fn = true,
           css = true,
           css_fn = true,
-          mode = 'background',     -- Fond coloré
-          virtualtext = '',      -- Icône carrée
+          mode = "background", -- Fond coloré
+          virtualtext = "", -- Icône carrée
           -- virtualtext = '■',    -- Alternative
-          always_update = true,    -- Mise à jour en temps réel
+          always_update = true, -- Mise à jour en temps réel
         },
         -- Tous les formats supportés :
         buftypes = {
-          '*',
-          '!prompt',
-          '!popup',
+          "*",
+          "!prompt",
+          "!popup",
         },
       })
 
       -- Activer immédiatement pour tous les buffers ouverts
       vim.defer_fn(function()
-        require('colorizer').attach_to_buffer(0)
+        require("colorizer").attach_to_buffer(0)
       end, 0)
     end,
-  }
+  },
 }
